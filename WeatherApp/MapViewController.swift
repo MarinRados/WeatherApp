566bc781eb4e58be73217274671f9ca8b40ec135
Marin Rados <marinrados@Marins-MacBook-Pro.local>
2017-03-27 16:36:20 +0200

    getting coordinates from map

--- WeatherApp/MapViewController.swift
+++ WeatherApp/MapViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.mapType = .hybrid
+            mapView.delegate = self
+            let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.getLocationWith(gesture:)))
+            gestureRecognizer.minimumPressDuration = 1.0
+            gestureRecognizer.delaysTouchesBegan = true
+            gestureRecognizer.delegate = self
+            self.mapView.addGestureRecognizer(gestureRecognizer)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func getLocationWith(gesture: UILongPressGestureRecognizer) {
+        if gesture.state != UIGestureRecognizerState.began {
+            return
+        }
+        
+        let touchLocation = gesture.location(in: mapView)
+        let locationCoordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
+        print("Latitude \(locationCoordinate.latitude), Longitude \(locationCoordinate.longitude)")
     }
 
 }
